bc4e342edabd2d00df8c05fc54107b1f615d39df
RicardoTrinidad <76927713+RicardoTrinidad@users.noreply.github.com>
2021-01-13 06:30:52 -0600

    Create usuarios.php

---
+++ admin/usuarios.php
@@ -0,0 +1,30 @@
+<?php include("header.php"); ?>
+      <div class="container" id="container5">
+      <h1>Usuarios</h1>
+     
+      
+	<table class="table" border="1" >
+		<tr>
+			
+			<td>Nombre</td>
+			<td>Apellidos</td>
+            <td>
+            Edad
+            </td>
+            <td>
+            username
+            </td>
+            <td>
+            eliminar 
+            </td>
+				
+		</tr>
+
+		
+	</table>
+   
+    
+
+</form>
+  </body>
+</html>
